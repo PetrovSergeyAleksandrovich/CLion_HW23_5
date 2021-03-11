556934c4da53090ea249ad401e56d2a1bcdab8cd
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-03-11 18:45:36 +0300

    HW_23_5_task_2 done

--- HW_23_5_task_2.cpp
+++ HW_23_5_task_2.cpp
@@ -4,13 +4,26 @@
 #include <ctime>
 #include <Windows.h>
 
-int randomizer()
+int randomizer_x()
+{
+    int x = 0;
+    srand ( time(NULL) );
+    for(int i = 0; i < rand() % 5; i++)
+    {
+        x += rand() % 5;
+        if(!x) {i=0;continue;}
+    }
+    return x;
+}
+
+int randomizer_y()
 {
     int x = 0;
     srand ( time(NULL) );
     for(int i = 0; i < rand() % 10; i++)
     {
-        x += rand() % 10;
+        x += rand() % 5;
+        if(!x) {i=0;continue;}
     }
     return x;
 }
@@ -26,20 +39,27 @@ int bool_randomizer(int times)
 int main()
 {
     int
-    x = randomizer(),
-    y = randomizer(),
-    rand_number = randomizer();
+    x = randomizer_x(),
+    y = randomizer_y(),
+    rand_number = randomizer_x() + randomizer_y();
 
-    while(true)
+    std::ofstream file;
+    file.open("C:\\Users\\Poizone\\CLion_HW23_5\\bool_picture.txt");
+
+    std::cout << "string length x=" << x << " column height y=" << y << std::endl << std::endl;
+    for(int i = 0; i < x; i++)
     {
-        for(int i = 0; i < randomizer(); i++)
+        for(int j = 0; j < y; j++)
         {
-            std::cout << bool_randomizer(rand_number) << " ";
-            Sleep(1001);
+            int w = bool_randomizer(rand_number);
+            std::cout << w << " ";
+            file << w << " ";
+            Sleep(100);
         }
-
+        std::cout << "\n";
+        file << "\n";
     }
-
+    file.close();
 
     return 0;
 }
